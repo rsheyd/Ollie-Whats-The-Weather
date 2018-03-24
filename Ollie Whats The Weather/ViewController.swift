0241ef4276ffad17a5fb93a0381c3aaf312e4a92
Roman Sheydvasser <s.roman@gmail.com>
2018-03-24 10:32:09 -0400

    Initial Commit

---
+++ Ollie Whats The Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ollie Whats The Weather
+//
+//  Created by Roman Sheydvasser on 3/24/18.
+//  Copyright © 2018 RLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
